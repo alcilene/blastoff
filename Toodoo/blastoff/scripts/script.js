ef302e43d5813987215c90e5fa2bd66577ce78a1
Alcilene Reis <reis.alcilene@gmail.com>
2022-06-15 11:08:11 -0300

    Teste BlastOff

---
+++ Toodoo/blastoff/scripts/script.js
@@ -0,0 +1,37 @@
+function umAlerta(){
+    alert("Este é o Teste de Web BlastOff!");
+}
+
+var cumprimentar = "Hello World!";
+var cumprimentar = "Hello Toodoo!";
+
+console.log(cumprimentar);
+
+//------------------------------------------------------------
+
+  
+ var sabor = "chocolate";
+
+ if (sabor == "chocolate") {
+     alert("Amo sorvete de chocolate!");
+}else {
+    alert("Prefiro outros sabores.");
+        
+} 
+
+//--------------------------------------------------------------
+
+// function multiplicacao (num1,num2) { 
+//     let resultado = num1 * num2; 
+//     return resultado; 
+//    } 
+
+//    console.log(multiplicacao(4,7)); // Resultado = 28
+//    console.log(multiplicacao(5,6)); // Resultado = 30
+//    console.log(multiplicacao(3,3)); // Resultado = 9
+
+   
+
+
+
+
